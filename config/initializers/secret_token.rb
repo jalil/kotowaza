15b5de193289f7762fff6970308999254d764387
jalil mohammed <jalilsan@gmail.com>
2013-06-17 00:11:05 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kotowaza::Application.config.secret_key_base = 'fda3f9b255909fbe1fa4c91d5aef865ecc03bd0d429e638d9568a908d63412231c8eff74cb656d64a2193a2d4c01e506a8744112e21bc2f986365fe78bddd741'
